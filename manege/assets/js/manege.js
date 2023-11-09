43b88477b024ecb15b0031ecda0296063b062d6b
Matteoninis <msimonetti@edenschool.fr>
2023-11-09 17:09:44 +0100

    manege v1.1

--- manege/assets/js/manege.js
+++ manege/assets/js/manege.js
@@ -1,19 +1,6 @@
-let i = 1;
-while ( i <= 10 ) {
-    console.log('Tour n° ' + i);
-    i++;
+let tamere = prompt("combien de tour?")
+let i = 1
+while(i <tamere) {
+  console.log("C'est le tour de manège n° " + i)
+  i++;
 }
-for ( let j = 100; j <= 110; j++){
-   console.log('Tour n° ' + j);
-}
-for (let j = 100; j <= 110; j++) {
-    console.log('Tour n° ' + j);
-}
-for (let i = 1 ; i <= 10; i++) {
-    if ( i < 6 ){
-      document.write('<p style="color: red;">Tour n°' + i + '</p>');
-    } else {
-    document.write('<p style="color: blue;">Tour n°' + i + '</p>')
-  }
-}
-
